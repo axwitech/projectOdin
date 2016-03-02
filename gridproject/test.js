29e7c7e4be6de84eccf2b4b2340ec35c5a9dfccc
Vladimir <axwitech@gmail.com>
2016-03-02 11:39:21 +0200

    working minimum
    clearing grid is not working in order to get new grid refresh page

--- gridproject/test.js
+++ gridproject/test.js
@@ -1,25 +1,34 @@
 var rows;
 var colums;
+var newHeight;
+var newWidth;
 var $row = $("<div />", {class: 'row'});
 var $square = $("<div />", {class: 'square'});
 
 $(document).ready(function(){
-   
 
 	//$(".square").hover(function(){
-	 //	  $( this ).addClass("hover");
-	
-  //});
-
+	//$( this ).addClass("hover");
+  	
 });
 
 function generateGrid(x,y){
-	 for(var i = 0; i < x; i++){      
+	
+      
+	 for(var i = 0; i < x; i++){
+
             $("#wrapper").append($row.clone());
+            $(".row").height(newHeight); 
+             
     }
 
     for(var i = 0; i < y; i++){
+    		
             $(".row").append($square.clone());
+            $(".square").height(newHeight);
+			$(".square").width(newWidth);
+            
+		
     }
 
 }
@@ -28,8 +37,31 @@ function clear(){
 
 }
 
+function effects(){
+	$(".square").hover(function(){
+	$( this ).addClass("hover");
+	});
+}
+
+	
+
 function promptGrid(){
 	rows = prompt("How many rows?");
 	columns = prompt("How many columns?");
+	resizeToFit(rows,columns);
 	generateGrid(rows, columns);
+	effects();
+}
+function resizeToFit(x,y){
+	var wrapperHeight = $("#wrapper").height();
+	console.log(wrapperHeight);
+	var wrapperWidth = $("#wrapper").width();
+	console.log(wrapperWidth);
+	newHeight = Math.round(wrapperHeight/x);
+	console.log(newHeight);
+	newWidth = Math.round(wrapperWidth/y);
+	console.log(newWidth);
+	
+	
+	
 }

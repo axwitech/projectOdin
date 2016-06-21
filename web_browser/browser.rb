e0a62a6b8e99c0dd074260bb35a782c3d34e2fd0
Vladimir <axwitech@gmail.com>
2016-06-21 13:57:08 +0300

    browser and server update

--- web_browser/browser.rb
+++ web_browser/browser.rb
@@ -1,14 +1,17 @@
 require 'socket'
-require "server"
 
-host = 'www.tutorialspoint.com'     # The web server
-port = 80                           # Default HTTP port
-path = "/index.htm"                 # The file we want
+host = 'localhost'     # The web server
+port = 2000                        # Default HTTP port
+path = "/index.html"                 # The file we want
 
 # This is the HTTP request we send to fetch a file
 request = "GET #{path} HTTP/1.0\r\n\r\n"
 
 socket = TCPSocket.open(host,port)  # Connect to server
+puts "Made a connection to server"
+while line = socket.gets   # Read lines from the socket
+  puts line.chop      # And print with platform line terminator
+end 
 socket.print(request)               # Send request
 response = socket.read              # Read complete response
 # Split response at first blank line into headers and body
--- web_browser/server.rb
+++ web_browser/server.rb
@@ -1,10 +1,12 @@
 require 'socket'               # Get sockets from stdlib
-require "browser"
+response_body = File.read("./index.html")
+
 
 server = TCPServer.open(2000)  # Socket to listen on port 2000
 loop {                         # Servers run forever
   client = server.accept       # Wait for a client to connect
   client.puts(Time.now.ctime)  # Send the time to the client
+  client.puts response_body
   client.puts "Closing the connection. Bye!"
   client.close                 # Disconnect from the client
 }

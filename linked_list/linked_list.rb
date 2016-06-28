5286df3a142bda8df3bab278f321e2fc7cd11837
Vladimir <axwitech@gmail.com>
2016-06-28 13:17:33 +0300

    something to a node

--- linked_list/linked_list.rb
+++ linked_list/linked_list.rb
@@ -1,5 +1,17 @@
 class LinkedList
+
+
+
 end
 
 class Node
+	attr_accessor :head
+	attr_accessor :tail
+	attr_accessor :value
+
+	def init(head, tail, value)
+		@head = head
+		@tail = tail
+		@value = value
+	end
 end

4d8a0886a7643ef5770fefe5990d2a4087fef9b2
Vladimir <axwitech@gmail.com>
2016-07-03 21:28:31 +0300

    append done on linked list

--- linked_list/linked_list.rb
+++ linked_list/linked_list.rb
@@ -1,17 +1,38 @@
-class LinkedList
-
-
-
-end
-
 class Node
-	attr_accessor :head
-	attr_accessor :tail
-	attr_accessor :value
+  attr_accessor :next
+  attr_accessor :value
 
-	def init(head, tail, value)
-		@head = head
-		@tail = tail
-		@value = value
-	end
+  def initialize(value)
+    @value = value
+    @next = nil
+  end
 end
+
+class LinkedList
+  attr_reader :size
+  def initialize
+    @head = nil
+    @tail = nil
+    @size = 0
+  end
+  def append(data)
+  	@size += 1
+    node = Node.new(data)
+    if @head.nil?
+      @head = node
+      @tail = @head
+    else
+      @tail.next = node
+      @tail = @tail.next
+    end
+
+  end
+
+end
+
+
+
+
+ll = LinkedList.new
+ll.append("2")
+ll.append("3")

d7dc77de4b5c9dd13b2246bf4d1cfd94eef67802
Vladimir <axwitech@gmail.com>
2016-12-26 22:15:33 +0200

    postponed finishing ruby
    Started learning sinatra, completed first assigment

--- binary_search_tree/bst.rb
+++ binary_search_tree/bst.rb
@@ -8,6 +8,7 @@ class Node
     @parent = parent
     @child = child
   end
+  
 end
 array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 $tree = []
---
+++ sinatra/hi.rb
@@ -0,0 +1,22 @@
+require 'sinatra'
+require 'sinatra/reloader'
+$number = rand(101)
+$message = 'Try to guess'
+   get '/' do
+     erb :index, :locals => {:number => $number, :message => $message}
+     if params['guess'] != nil
+       check(params['guess'])
+     end
+     erb :index, :locals => {:number => $number, :message => $message}
+   end
+
+def check(guess)
+  guessednumber = Integer(guess)
+  if (guessednumber > $number)
+     return $message = 'Too high'
+  elsif (guessednumber < $number)
+     return $message = 'Too low'
+   elsif (guessednumber == $number)
+     return $message = "GG"
+   end
+end
